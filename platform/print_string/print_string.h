d978c154da3cc39cbc3e3115eba51ec635a3e12b
Sephiroth <sephirothictreevii@gmail.com>
2019-07-16 16:08:13 +0800

    阶段性初步功能完成-暂存

--- platform/print_string/print_string.h
+++ platform/print_string/print_string.h
@@ -5,16 +5,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define PRINT_STR_POOL_ITEM_SIZE sizeof(print_str_t)
-#define PRINT_STR_POOL_ITEM_SIZE 64
-#define PRINT_STR_POOL_ITEM_COUNT 8
-
-typedef struct print_str_pool_item_t print_str_pool_item_t;
-struct print_str_pool_item_t {
-    bool bIsFree;
-    uint8_t chBuffer[PRINT_STR_POOL_ITEM_SIZE];
-};
-
 typedef bool print_byte_t(void *, uint8_t);
 
 typedef struct {
@@ -34,9 +24,18 @@ typedef struct {
     #endif
 } print_str_cfg_t;
 
-static print_str_pool_item_t s_tExamplePool[PRINT_STR_POOL_ITEM_COUNT];
+#define PRINT_STR_POOL_ITEM_SIZE sizeof(print_str_t)
+#define PRINT_STR_POOL_ITEM_COUNT 8
+
+typedef struct print_str_pool_item_t print_str_pool_item_t;
+struct print_str_pool_item_t {
+    bool bIsFree;
+    uint8_t chBuffer[PRINT_STR_POOL_ITEM_SIZE];
+};
+
+static print_str_pool_item_t s_tPrintStringPool[PRINT_STR_POOL_ITEM_COUNT];
 print_str_pool_item_t *print_str_pool_allocate(void);
-void xxxx_pool_free(print_str_pool_item_t *ptItem);
+void print_str_pool_free(print_str_pool_item_t *ptItem);
 
 extern fsm_rt_t print_string(print_str_t *ptThis);
 extern bool print_string_init(print_str_t *ptThis, const print_str_cfg_t *ptCFG);
